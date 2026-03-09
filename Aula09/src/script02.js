103ea66592b4d03ee2e69a738807029481a94d97
¨Jhenifer <Contanovajheni2@gmail.com>
2026-03-09 20:22:20 -0300

    Aula 9 - Popriedades de Number em Javascript

---
+++ Aula09/src/script02.js
@@ -0,0 +1,34 @@
+// Utilização da propriedade
+EPSILON
+let a = Number.EPSILON;
+console.log(a);
+
+//Utilização da propriedade
+MAS_VALUE
+let b = Number.MAX_VALUE;
+console.log(b);
+
+//Utilização da propriedade
+Min_VALUE
+let c = Number.MAX_VALUE;
+console.log(c);
+
+//Utilização da propriedade Min_safe_INTEGER
+let d = Number.MAX_VALUE;
+console.log(d);
+
+//Utilização da propriedade MAX_safe_INTEGER
+let e = Number.MAX_VALUE;
+console.log(e);
+
+//Utilização da propriedade POSITIVE_INFINITY
+let f = Number.POSITIVE_INFINITY;
+console.log(f);
+
+// utilização da propriedade NEGATIVE_INFINITY
+let g = Number.NEGATIVE_INFINITY;
+console.log(g);
+
+// utilização da propriedade NEGATIVE_INFINITY
+let h = Number.NaN;
+console.log(h);
