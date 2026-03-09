51f22c01470f6a2641d786b9f7688e839e66d2a7
¨Jhenifer <Contanovajheni2@gmail.com>
2026-03-09 20:00:51 -0300

    Aula 9 - Métodos de Número em JavaScript

---
+++ Aula06/src/script02.js
@@ -0,0 +1,12 @@
+//Exemplo do operador NOT
+let usuariologado = false;
+
+if (!usuariologado) {
+    //console.log(`teste`);
+}
+
+let lista = [];
+
+if (!lista.length) {
+    console.log('A lista está vazia');
+}
---
+++ Aula09/src/script.js
@@ -0,0 +1,29 @@
+// Utilização do método toString()
+let numero = 123;
+let exToString = numero.toString();
+console.log(typeof exToString);
+
+//Utilização do método toExponential()
+let a = 9.646;
+let toExponential = a.toExponential(2);
+console.log(exToExponential);
+
+//Utilização do toFixed()
+let b = 77.777;
+let exTofixed = b.toFixed(7);
+console.log(exTofixed);
+
+// Utilização do método toPrecision()
+let c = 63.87389232;
+let exToPrecision = c.toPrecision(4);
+console.log(typeof  exToPrecision);
+
+// Utilização do método valueOf()
+let d = 2006;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+// Utilização do método Number()
+let e = "593";
+let exNumber =(e);
+console.log(exNumber);
