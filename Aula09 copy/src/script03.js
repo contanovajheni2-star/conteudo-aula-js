3c3c33603b16c0f2cc6840d9d5e7fce506dc2b85
¨Jhenifer <Contanovajheni2@gmail.com>
2026-03-11 22:10:25 -0300

    Aula 11 - Estudando Objetos

---
+++ Aula09 copy/src/script03.js
@@ -0,0 +1,26 @@
+// Utilizando BigInt
+let a = BigInt
+("1822929388918272737282828838219911928388288282373821818928283238737967687678678678678676779879888979878979");
+console.log(a);
+
+//Utilizando n
+let b = 
+57588493922292922883838383292929292922828228282882828828288288282828828282373737447565839299191928282n;
+console.log(b);
+
+// Utilizando operadores aritméticos com BigInt
+let c = 46382748294782937n;
+let d = 74389276448847658n;
+let e = (c + d);
+console.log(e);
+
+// Convertendo BigInt
+let f = BigInt("7584397493748273483827");
+let fConvertido = Number();
+console.log(fConvertido);
+
+// Utilização métodos de comparação em BigInt
+let g = 100n;
+let h = 90n;
+let exComparacao = (g > h);
+console.log(exComparacao);
---
+++ Aula11/src/script03.js
@@ -0,0 +1,21 @@
+// Utilização o this em função
+
+function teste() {
+    return console.log(this);
+}
+
+teste();
+
+const carro = {
+    marca:"Pagani",
+    modelo:"Huayra",
+    cor:"Amarelo",
+}
+
+let carroArry = Object.values();
+(carro);
+
+let cArrayString = carroArray;
+toString();
+
+console.log(carroArray);
