fe5646573cef2fe0c0cd2d12a80274b80f041851
¨Jhenifer <Contanovajheni2@gmail.com>
2026-03-23 22:08:48 -0300

    Aula 18 - Utilizando metodos de tempo

--- Aula 18/src/script02.js
+++ Aula 18/src/script02.js
@@ -1,22 +1,48 @@
-// Utilizando o evento mouseover
-const caixa = document.querySelector(".caixa");
+// Utilizando o método setInterval()
+const elemento = document.querySelector(".meu-relogio");
 
-caixa.addEventListener("mouseover", function (){
-    caixa.innerHTML = "O mouse esta em cima da caixa";
-    caixa.style.background = "blue";
+setInterval(mostrarRelogio, 1000);
+
+function mostrarRelogio() {
+  const data = new Date();
+  const horas = data.getHours();
+  const minutos = data.getMinutes();
+  const segundos = data.getSeconds();
+
+  elemento.innerHTML = `${horas} : ${minutos} : ${segundos}`;
+}
+
+// Utilizando o método setTimeout()
+const conteudo = document.querySelector(".meu-conteudo");
+const botao = document.querySelector(".elemento-botao");
+
+botao.addEventListener("click", handlerTimeout);
+
+function handlerTimeout() {
+    setTimeout(mostrarConteudo, 2000);
+}
+
+function mostrarConteudo() {
+    conteudo.innerHTML = `Esse texto está aparecendo após dois segundos da interação com o botão`;
+}
+
+// Utilizando o método setInterval
+const exibirNumeros = document.querySelector(".conteudo-contador");
+const botaoInicio = document.querySelector(".botao-inicio");
+const botaoPausar = document.querySelector(".botao-pausar");
+
+let intervalo;
+let contador = 0;
+
+botaoInicio.addEventListener("click", () => {
+ intervalo = setInterval(contadora, 1000);
 });
 
-caixa.addEventListener("mouseout", function () {
-    caixa.innerHTML = "O mouse esta fora da caixa.";
-    caixa.style.background = "darkblue";
-    caixa.style.color = "#ffffff";
+botaoPausar.addEventListener("click", () => {
+ clearInterval(intervalo);
 });
 
-// Exemplo de posição do mouse na página
-const conteudo = document.querySelector(".conteudo");
-
-document.addEventListener("mousemove", handlePosicao);
-
-function handlePosicao(evento) {
-    conteudo.innerHTML = "X: " + evento.clientX + " " + "Y: " + evento.clientY;
+function contadora() {
+    contador++;
+    exibirNumeros.innerHTML = contador;
 }
